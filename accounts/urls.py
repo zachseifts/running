810defbe7267cc8e9f04b8a558d3625475408ebc
Zach Seifts <zach.seifts@gmail.com>
2022-05-23 18:52:37 -0400

    cleaining stuff up

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views
 from .views import AccountsProfileView
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name = 'login.html'), name='accounts-login'),
-    path('logout/', views.LogoutView.as_view(template_name = 'logout.html'), name='accounts-logout'),
-    path('profile/', AccountsProfileView.as_view(), name = 'accounts-profile'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='accounts-login'),
+    path('logout/', views.LogoutView.as_view(template_name='logout.html'), name='accounts-logout'),
+    path('profile/', AccountsProfileView.as_view(), name='accounts-profile'),
 ]
