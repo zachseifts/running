e22a8d7e4916ffc727ec3fdf7440e21a31283879
Zach Seifts <zach.seifts@gmail.com>
2022-05-24 07:36:59 -0400

    redirecting homepage to profile when a user is logged in

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.views import View
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 class HomeView(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('accounts-profile')
         context = {}
         return render(request, 'home.html', context)
         
