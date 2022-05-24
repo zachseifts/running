1fb12d210dde5c3022f61fb1e2eda0eaddc3c1e8
Zach Seifts <zach.seifts@gmail.com>
2022-05-24 08:23:50 -0400

    changing the url of the activity detail page

--- activities/urls.py
+++ activities/urls.py
@@ -5,5 +5,5 @@ from .views import LocationCreateView, ActivityCreateView, ActivityDetailView
 urlpatterns = [
     path('create/location', LocationCreateView.as_view(), name='create-location'),
     path('create/activity', ActivityCreateView.as_view(), name='create-activity'),
-    path('activity/<int:activity_id>/', ActivityDetailView.as_view(), name='activity-detail'),
+    path('<int:activity_id>/', ActivityDetailView.as_view(), name='activity-detail'),
 ]
